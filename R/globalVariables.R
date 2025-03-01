2538845bbe1026adb6ee694239cbb5aa3d28ee36
tojupeix@gmail.com <tojupeix@gmail.com>
2025-03-01 14:35:25 +0100

    Finalisation du package avec tests et documentation

---
+++ R/globalVariables.R
@@ -0,0 +1,16 @@
+utils::globalVariables(c(
+ "validate_schema",
+ "Code.de.la.categorie.socio.professionnelle",
+ "n",
+ "desc",
+ "reorder",
+ "Libelle.de.la.commune",
+ "Date.de.naissance",
+ "Nom.de.l.elu",
+ "Prenom.de.l.elu"
+))
+utils::globalVariables(c(
+ "Code.de.la.categorie.socio.professionnelle",
+ "Libelle.de.la.commune"
+))
+
